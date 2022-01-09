c7559fec8e4946ebd946102133e3ee51d158d2ca
Jakob845 <jakobzetterqvist@hotmail.com>
2022-01-09 10:25:39 +0100

    Add project files.

---
+++ CV_Webbutveckling/Controllers/AboutMeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Webbutveckling.Controllers
+{
+    public class AboutMeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
